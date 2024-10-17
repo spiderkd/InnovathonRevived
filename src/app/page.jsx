0737e5969e4479ec35a74db86f591c19bd632ff6
Sachin kumar gupta <sachinkg1309@gmail.com>
2024-10-17 06:15:14 +0530

    added timeline section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,14 +3,16 @@ import Hero from "@/components/Hero/Hero";
 
 import { SlideTabsExample } from "@/components/Navbar/Navbar";
 import Slider from "@/components/SliderNext/SliderMain";
+import { TimelineDemo } from "@/components/TimelineMain/TimelineMain";
 
 export default function Home() {
-  return (
-    <div>
-      <SlideTabsExample />
-      <Hero />
-      <Slider />
-      <Noise />
-    </div>
-  );
+	return (
+		<div>
+			<SlideTabsExample />
+			<Hero />
+			<TimelineDemo />
+			<Slider />
+			<Noise />
+		</div>
+	);
 }
